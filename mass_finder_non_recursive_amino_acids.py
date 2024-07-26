702b1758e5afb32ea682afa63b7d5a57aeb1a942
JohannesE <jeckert@ethz.ch>
2024-07-26 09:05:07 +0200

    undo rewrite the general variables and the pool part to be separate and easier to follow

--- mass_finder_non_recursive_amino_acids.py
+++ mass_finder_non_recursive_amino_acids.py
@@ -593,9 +593,6 @@ def main():
     mass_range = [float(mass) for mass in args.mass_range.split('-')]
     time_range = [float(time) for time in args.time_range.split('-')]
     formulas_with_charge = generate_formula_with_charge(generate_formulas(args.elements), mass_range, args.monoisotopic)
-    plot_time_range = [round(float(data.time[float(time)]['retentionTime']), 2) for time in
-                       args.plot_time_range.split('-')]
-    plot_mass_range = [float(mass) for mass in args.plot_mass_range.split('-')]
 
     # Check if the plot_time_range and plot_mass_range are set to the default values
     # If not, set full_range to True, since full_range is needed to trigger personal plot time and mass ranges
@@ -664,7 +661,9 @@ def main():
 
         # Plot the results
         # Check if the full range is set to True
-
+        plot_time_range = [round(float(data.time[float(time)]['retentionTime']), 2) for time in
+                           args.plot_time_range.split('-')]
+        plot_mass_range = [float(mass) for mass in args.plot_mass_range.split('-')]
         plot_results(analyzed_spectra, os.path.splitext(file)[0], plot_time_range, plot_mass_range,
                            args.overwrite, full_range, args.min_intensity, args.plot_group_identifiers)
     pool.close()
